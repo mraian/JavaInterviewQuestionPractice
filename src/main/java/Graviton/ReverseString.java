1a8b16c1976ab0db22eccea0c530b89f71c90db6
M Raian <mraian@Ms-MacBook-Pro.local>
2021-08-28 02:13:33 -0400

    added return type

--- src/main/java/Graviton/ReverseString.java
+++ src/main/java/Graviton/ReverseString.java
@@ -2,16 +2,18 @@ package Graviton;
 
 public class ReverseString {
     public static void main(String [] args){
-        reverseString("Hola Papi");
+        System.out.println(reverseString("Hola Papi"));
+
 
     }
 
-    public static void reverseString(String input){
+    public static String reverseString(String input){
         String output="";
 
         for(int i=input.length()-1; i>=0; i--){
             output = output+input.charAt(i);
         }
-        System.out.println(output);
+        //System.out.println(output);
+        return output;
     }
 }
